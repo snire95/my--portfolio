993d613c708594e434c7f215a9ad354aa9711f66
snire95 <snire995@gmail.com>
2021-05-23 11:53:37 +0300

    updating1

--- src/components/Post.js
+++ src/components/Post.js
@@ -24,7 +24,7 @@ export default function Post() {
    <section className="container mx-auto">
     <h1 className="text-5xl flex justify-center cursive">Blog Posts Page</h1>
     <h2 className="text-lg text-gray-600 flex justify-center mb-12">welcome to my page of blog posts</h2>
-    <div className="grid mb:grid-cols-2 lg:grid-cols-3 gap-8">
+    <div className="grid mb:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8">
      {postData && postData.map((post,index) => (
      <article>
       <Link to={"/post/" + post.slug.current} key={post.slug.current}>
