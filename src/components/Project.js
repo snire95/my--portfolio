5817936948bdd85ff7552af0e842a2b4c9c93884
snire95 <snire995@gmail.com>
2021-05-23 11:48:04 +0300

    updating

--- src/components/Project.js
+++ src/components/Project.js
@@ -23,7 +23,7 @@ export default function Project() {
     <h2 className = "text-lg text-gray-600 flex justify-center mb-12">
      Welcome to my projects page!
      </h2>
-    <section className= "grid grid-cols-2 gap-8">
+    <section className= "grid mb:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-8">
      {projectData && projectData.map((project, index ) => (
      <article className="relative rounded-lg shadow-xl bg-white p-16">
       <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-red-700">
